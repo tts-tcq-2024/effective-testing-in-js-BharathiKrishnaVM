b25b095c9225db59e413c9288780b62762616b32
Bharathi Krishna V M <49585360+BharathiKrishnaVM@users.noreply.github.com>
2024-10-11 11:22:50 +0530

    Update tshirts.js

--- tshirts.js
+++ tshirts.js
@@ -13,4 +13,6 @@ function size(cms) {
 expect(size(37)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
+
+expect(size(38)).equals('S'); //Fail test case
 console.log('All is well (maybe!)');
